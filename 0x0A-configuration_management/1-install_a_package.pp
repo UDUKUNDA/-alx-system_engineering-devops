a403d89df4d13413a622f2a08c5d9c7477e0d2ca
UDUKUNDA <udukudndaelysee@gmail.com>
2024-03-12 09:44:40 +0000

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
 #Using Puppet  to, install puppet-lint.
 
+class { 'python':
+  version => 'system',
+}
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'Flask':
   ensure  => '2.1.0',
   provider => 'pip3',
+  require => Package['python3-pip'],
 }
+
